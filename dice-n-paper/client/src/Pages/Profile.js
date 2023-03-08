8011bf41e3d9e75879c9c2f605c14aba17630b2c
TGray95 <gray.tony95@gmail.com>
2023-03-07 21:40:04 -0500

    profile page now automatically retrieves logged in user's info

--- dice-n-paper/client/src/Pages/Profile.js
+++ dice-n-paper/client/src/Pages/Profile.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { useQuery } from '@apollo/client'
-import { QUERY_ME } from '../utils/queries';
+import { QUERY_ME, QUERY_USER } from '../utils/queries';
 import Auth from '../utils/auth'
+import { useParams } from 'react-router-dom';
   
   const Profile = () => {
-    const {loading, data} = useQuery(QUERY_ME,)
+    const {userId }= useParams()
+    const { loading, data } = useQuery(
+      userId ? QUERY_USER : QUERY_ME,
+      {
+        variables: { userId: userId },
+      }
+    );
 
-    const user = data?.me || {};
-    console.log(user);
-    console.log(Auth.loggedIn())
+  
+  
     if (loading) {
       return <div>Loading profile...</div>
     }
+    const user = data?.me || data?.user || {};
+  
     return (
       <div className="Profile">
         <img src="" alt="Profile Image" />
         <h2>username: {user.username}</h2>
         <h4>Location</h4>
-        <h4>Games: games</h4>
-        <p>Bio: test bio</p>
+        <h4>Games: {user.profile.games}</h4>
+        <p>Bio: {user.profile.bio}</p>
+        <p>Friends: {user.friends}</p>
       </div>
     );
   }
